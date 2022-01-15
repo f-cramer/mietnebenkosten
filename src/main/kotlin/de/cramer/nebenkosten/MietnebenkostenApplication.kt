3508c1e637f39437355bc1ba572c05a0394ed3a8
Florian Cramer <f.cramer@superus.de>
2022-01-15 21:39:07 +0100

    Logger-Bean-Definition in eigene Konfiguration verschoben

--- src/main/kotlin/de/cramer/nebenkosten/MietnebenkostenApplication.kt
+++ src/main/kotlin/de/cramer/nebenkosten/MietnebenkostenApplication.kt
@@ -1,24 +1,10 @@
 package de.cramer.nebenkosten
 
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.InjectionPoint
-import org.springframework.beans.factory.config.ConfigurableBeanFactory.*
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Scope
 
 @SpringBootApplication
-class MietnebenkostenApplication {
-
-    @Bean
-    @Scope(SCOPE_PROTOTYPE)
-    fun logger(injectionPoint: InjectionPoint): Logger {
-        val type = injectionPoint.member.declaringClass
-        return LoggerFactory.getLogger(type)
-    }
-}
+class MietnebenkostenApplication
 
 fun main(args: Array<String>) {
     runApplication<MietnebenkostenApplication>(*args)
