892173812b697765d56d12dbf05b928a6c87e49b
Florian Cramer <f.cramer@superus.de>
2022-01-16 02:34:06 +0100

    Unterstützung für Locale-Änderung via Request-Parameter

--- src/main/kotlin/de/cramer/nebenkosten/config/WebConfiguration.kt
+++ src/main/kotlin/de/cramer/nebenkosten/config/WebConfiguration.kt
@@ -2,10 +2,14 @@ package de.cramer.nebenkosten.config
 
 import de.cramer.nebenkosten.config.year.YearChangeInterceptor
 import de.cramer.nebenkosten.config.year.YearResolver
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.LocaleResolver
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor
+import org.springframework.web.servlet.i18n.SessionLocaleResolver
 
 @Configuration
 class WebConfiguration(
@@ -19,5 +23,9 @@ class WebConfiguration(
 
     override fun addInterceptors(registry: InterceptorRegistry) {
         registry.addInterceptor(yearChangeInterceptor)
+        registry.addInterceptor(LocaleChangeInterceptor())
     }
+
+    @Bean
+    fun localeResolver(): LocaleResolver = SessionLocaleResolver()
 }
