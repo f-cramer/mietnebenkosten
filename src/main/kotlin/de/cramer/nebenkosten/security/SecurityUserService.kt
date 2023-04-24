883c4fa246c824cd83d9d32469880886bf70694b
Florian Cramer <florian.cramer.112@gmail.com>
2023-04-24 21:22:24 +0200

    SecurityUserService und SecurityUserPasswordService zusammengeführt

--- src/main/kotlin/de/cramer/nebenkosten/security/SecurityUserService.kt
+++ src/main/kotlin/de/cramer/nebenkosten/security/SecurityUserService.kt
@@ -2,6 +2,7 @@ package de.cramer.nebenkosten.security
 
 import de.cramer.nebenkosten.services.UserService
 import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsPasswordService
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
@@ -9,8 +10,21 @@ import org.springframework.stereotype.Service
 @Service
 class SecurityUserService(
     private val userService: UserService,
-) : UserDetailsService {
+) : UserDetailsService, UserDetailsPasswordService {
 
     override fun loadUserByUsername(username: String): UserDetails =
         userService.getUser(username)?.let { SecurityUser(it) } ?: throw UsernameNotFoundException(username)
+
+    override fun updatePassword(user: UserDetails, newPassword: String): UserDetails {
+        if (user is SecurityUser) {
+            user.user.also {
+                it.password = newPassword
+                userService.saveUser(it)
+
+                return SecurityUser(userService.getUser(it.username)!!)
+            }
+        }
+
+        return user
+    }
 }
