9fe940b4aa49127ff11094de2e28119215888fec
Florian Cramer <florian.cramer.112@gmail.com>
2023-04-25 00:20:28 +0200

    OpenEntityManagerInViewFilter registrieren

--- src/main/kotlin/de/cramer/nebenkosten/config/WebConfiguration.kt
+++ src/main/kotlin/de/cramer/nebenkosten/config/WebConfiguration.kt
@@ -2,8 +2,11 @@ package de.cramer.nebenkosten.config
 
 import de.cramer.nebenkosten.config.year.YearChangeInterceptor
 import de.cramer.nebenkosten.config.year.YearResolver
+import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.Ordered
+import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.servlet.LocaleResolver
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
@@ -28,4 +31,10 @@ class WebConfiguration(
 
     @Bean
     fun localeResolver(): LocaleResolver = SessionLocaleResolver()
+
+    @Bean
+    fun openEntityManagerInViewFilter(): FilterRegistrationBean<OpenEntityManagerInViewFilter> = FilterRegistrationBean<OpenEntityManagerInViewFilter>().apply {
+        filter = OpenEntityManagerInViewFilter()
+        order = Ordered.HIGHEST_PRECEDENCE + 2
+    }
 }
