97144eac97b1d270b685efe0c44776aa6dc82fcf
Florian Cramer <florian.cramer.112@gmail.com>
2023-04-30 16:14:07 +0200

    Versions-Management für Gradle Kotlin Plugins vereinfacht

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,11 +3,11 @@ rootProject.name = "nebenkosten"
 pluginManagement {
     plugins {
         val kotlinVersion = extra["KOTLIN_VERSION"] as String
-        id("org.jetbrains.kotlin.jvm") version kotlinVersion
-        id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
-        id("org.jetbrains.kotlin.plugin.jpa") version kotlinVersion
-        id("org.jetbrains.kotlin.plugin.allopen") version kotlinVersion
-        id("org.jetbrains.kotlin.kapt") version kotlinVersion
+        kotlin("jvm") version kotlinVersion
+        kotlin("plugin.spring") version kotlinVersion
+        kotlin("plugin.jpa") version kotlinVersion
+        kotlin("plugin.allopen") version kotlinVersion
+        kotlin("kapt") version kotlinVersion
 
         val springBootVersion = extra["SPRING_BOOT_VERSION"] as String
         id("org.springframework.boot") version springBootVersion
