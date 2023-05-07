0e5bd0d6522bf59fb6eab014cebf32af2915a090
Florian Cramer <florian.cramer.112@gmail.com>
2023-05-07 15:24:26 +0200

    Erzeugen von Spring Data Query-Methoden ausgeschaltet

--- src/main/kotlin/de/cramer/nebenkosten/MietnebenkostenApplication.kt
+++ src/main/kotlin/de/cramer/nebenkosten/MietnebenkostenApplication.kt
@@ -2,8 +2,11 @@ package de.cramer.nebenkosten
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.data.repository.query.QueryLookupStrategy
 
 @SpringBootApplication
+@EnableJpaRepositories(queryLookupStrategy = QueryLookupStrategy.Key.USE_DECLARED_QUERY)
 class MietnebenkostenApplication
 
 fun main(args: Array<String>) {
