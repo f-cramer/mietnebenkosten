f3a3c98c5c73d2f5d9560181d9d4c7023681e322
Florian Cramer <florian.cramer.112@gmail.com>
2023-07-12 23:56:48 +0200

    Gradle Wrapper Version im check-Task überprüfen

--- build.gradle.kts
+++ build.gradle.kts
@@ -115,6 +115,19 @@ tasks.named<DependencyUpdatesTask>("dependencyUpdates").configure {
     }
 }
 
+val checkGradleWrapperVersion: TaskProvider<Task> = tasks.register("checkGradleWrapperVersion") {
+    doFirst {
+        val currentVersion = GradleVersion.current()
+        val propertiesVersion = GradleVersion.version(project.extra["WRAPPER_VERSION"] as String)
+        if (currentVersion != propertiesVersion) {
+            throw GradleException("current gradle wrapper version (${currentVersion.version}) does not match property WRAPPER_VERSION (${propertiesVersion.version})")
+        }
+    }
+}
+tasks.named("check").configure {
+    dependsOn(checkGradleWrapperVersion)
+}
+
 dependencyManagement {
     configurations.getByName("detekt") {
         dependencies {
