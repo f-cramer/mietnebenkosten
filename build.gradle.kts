44143b07919ae5b8d7974eecb7c1d15a4f192df4
Florian Cramer <florian.cramer.112@gmail.com>
2026-03-21 14:10:36 +0100

    Dependabot-Gruppe für Kotlin hinzugefügt

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,11 +6,11 @@ import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 plugins {
     id("org.springframework.boot") version "4.0.2"
     id("io.spring.dependency-management") version "1.1.7"
-    kotlin("jvm") version "2.3.0"
-    kotlin("plugin.spring") version "2.3.0"
-    kotlin("plugin.jpa") version "2.3.0"
-    kotlin("plugin.allopen") version "2.3.0"
-    kotlin("kapt") version "2.3.0"
+    id("org.jetbrains.kotlin.jvm") version "2.3.0"
+    id("org.jetbrains.kotlin.plugin.spring") version "2.3.0"
+    id("org.jetbrains.kotlin.plugin.jpa") version "2.3.0"
+    id("org.jetbrains.kotlin.plugin.allopen") version "2.3.0"
+    id("org.jetbrains.kotlin.kapt") version "2.3.0"
 
     id("org.jlleitschuh.gradle.ktlint") version "14.0.1"
 //    id("io.gitlab.arturbosch.detekt") version "1.23.8"
